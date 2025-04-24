d70a69707cd24256269d705e0f512cdc214ebdce
Nasif Arafat <nasif@Nasifs-MacBook-Pro.local>
2025-04-25 04:23:32 +0530

    01and02Lessons

---
+++ 01BasicReactVite/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Basic Vite</h1>
+  )
+}
+
+export default App
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 5
+
+  const addValue = () => {
+    counter = counter + 1
+    if (counter > 10){
+      counter = 10
+    }
+    console.log('Value Added', counter);
+    setCounter(counter)
+    
+  }
+
+  const removeValue = () => {
+    counter = counter - 1
+    if (counter < 0){
+      counter = 0}
+    console.log('Value Removed', counter);
+    setCounter(counter)
+  }
+
+  return (
+    <>
+      <h2>Counter Value (Between 0 to 10): {counter}</h2>
+      <br />
+      <button
+      onClick={addValue}
+      >Add Value</button>
+      <br /><br />
+      <button
+      onClick={removeValue}
+      >Remove Value</button>
+    </>
+  )
+}
+
+export default App
